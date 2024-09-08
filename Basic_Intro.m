755112087d10284c3707d42f9e6b0310f5fbbf6c
ZeroCipherX <saqibkhank805@gmail.com>
2024-09-08 13:54:23 +0100

    Initial commit

---
+++ Basic_Intro.m
@@ -0,0 +1,41 @@
+% simple code to addition 
+
+clear all;
+clc;
+a=5;
+b=6;
+c=a+b;
+
+% comments introduction
+ 
+% Three dots comment
+
+ d = ...this is a comment 
+     9;
+
+ 
+fprintf(' %d, %d ', c, d);
+
+%multi line comments
+
+%{
+
+This all would be a comment between this bracket
+
+%}
+
+
+%% This section does some simple mathematics
+
+% Cell used to seperate the set of codes
+5+6
+
+%% Comment Short cut key
+
+% Ctrl t to comment 
+% Ctrl r to uncomment
+
+%% To run specific cell press 
+
+% Ctrl + Enter
+6+4
